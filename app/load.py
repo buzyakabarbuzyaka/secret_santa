ef62fde463cf3a6f7c92c8334ac3468cccc65d5d
Buzov Vyahceslav <buzovv1997@gmail.com>
2020-12-21 01:24:05 +0300

    working santa ver 0.1

--- app/load.py
+++ app/load.py
@@ -1,39 +1,86 @@
-import os
-import json
+import hashlib
 from os.path import join
+from app.settings import DATA_DIR
 import csv
-from typing import List
-
-FIND_ARR = []
-
-
-def index(path):
-    with open('index.csv', 'w', encoding='utf-8') as f:
-        f.write('id;title\n')
-        folder_list = os.listdir(path)
-        for folder in folder_list:
-            tmp_path = join(path, folder, 'data.json')
-            try:
-                with open(tmp_path, 'r', encoding="utf-8") as file_data:
-                    data = json.load(file_data)
-                data_id = data['id']
-                title = data['title']
-                tmp_string = f"{data_id};{title}\n"
-
-                f.write(tmp_string)
-            except Exception:
-                print(f"While processing {tmp_path} error occurred!")
-
-    with open("index.csv", encoding='utf-8') as csv_file:
-        reader = csv.DictReader(csv_file, delimiter=";")
-        for dict_line in reader:
-            FIND_ARR.append((dict_line['id'], dict_line['title'].lower()))
+from typing import Union, Tuple
+from random import choice
 
 
-def find(substr: str) -> List[str]:
-    substr = substr.lower()
-    id_arr = []
-    for data_id, title in FIND_ARR:
-        if substr in title:
-            id_arr.append(data_id)
-    return id_arr
+def hash_string(data: str):
+    return hashlib.sha256(str.encode(data)).hexdigest()
+
+
+def make_clear_obfuscated_dict(path=None):
+    if path is None:
+        path = DATA_DIR
+    with open(join(path, "list.csv"), encoding='utf-8') as csv_file:
+        raw_list = list(csv.reader(csv_file, delimiter=","))
+        return dict([(hash_string(t_id), '') for t_id, _ in raw_list])
+
+
+def update_from_out_file(obfuscated_dict, path=None):
+    if path is None:
+        path = DATA_DIR
+    file_path = join(path, "out.csv")
+
+    open(file_path, 'a').close()
+    with open(file_path, "r", encoding='utf-8') as csv_file:
+        raw_obfuscated_list = list(csv.reader(csv_file, delimiter=","))
+        obfuscated_dict.update(raw_obfuscated_list)
+
+
+def write_to_out_file(obfuscated_dict, path=None):
+    if path is None:
+        path = DATA_DIR
+    with open(join(path, "out.csv"), 'w', encoding='utf-8') as out:
+        for obfuscated_id, data in obfuscated_dict.items():
+            out.write(f"{obfuscated_id},{data}\n")
+
+
+def make_obfuscation_map(path=None):
+    if path is None:
+        path = DATA_DIR
+    with open(join(path, "list.csv"), encoding='utf-8') as csv_file:
+        raw_list = list(csv.reader(csv_file, delimiter=","))
+        obfuscation_map = dict([(hash_string(t_id), name) for t_id, name in raw_list])
+        return obfuscation_map
+
+
+def init(path=None):
+    obfuscated_dict = make_clear_obfuscated_dict(path)
+    update_from_out_file(obfuscated_dict, path)
+    write_to_out_file(obfuscated_dict, path)
+
+    return obfuscated_dict
+
+
+OBFUSCATION_MAP = make_obfuscation_map()
+_obfuscated_dict = init()
+
+
+def _choose_secret_name(hashed_id: str) -> str:
+    free_unique_names = set(OBFUSCATION_MAP.values())
+    free_unique_names -= set([OBFUSCATION_MAP[hashed_id]]) | set(_obfuscated_dict.values())
+    return choice(list(free_unique_names))
+
+
+def give_secret_name(telegram_id: str) -> Union[Tuple[bool, str], None]:
+    hashed_id = hash_string(telegram_id)
+    if hashed_id not in OBFUSCATION_MAP:
+        return None
+
+    if _obfuscated_dict[hashed_id]:
+        return True, _obfuscated_dict[hashed_id]
+    else:
+        secret_name = _choose_secret_name(hashed_id)
+        _obfuscated_dict[hashed_id] = secret_name
+        write_to_out_file(_obfuscated_dict)
+        return False, secret_name
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(_obfuscated_dict)
+    telegram_id = '4'
+    print(give_secret_name(telegram_id))
+
--- app/main.py
+++ app/main.py
@@ -1,5 +1,63 @@
 from app.settings import TOKEN, DATA_DIR
+import logging
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from app.load import give_secret_name
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Просто, блять, набери "хто?"')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text('Ди на хуй! (если не понятьно чекай /help)')
+
+
+def bind(update: Update, context: CallbackContext):
+    telegram_id = update.effective_chat.username
+    secret_answer = give_secret_name(telegram_id)
+
+    if not secret_answer:
+        update.message.reply_text('Ты не в банде, сорри(')
+    else:
+        is_already_given, secret_name = secret_answer
+        if is_already_given:
+            update.message.reply_text(f'Напоминаю, даришь подарок: {secret_name}')
+        else:
+            update.message.reply_text(f'Ты секретный санта для: {secret_name}')
+
+
+def main():
+    """Start the bot."""
+    updater = Updater(TOKEN, use_context=True)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    dispatcher.add_handler(MessageHandler(Filters.text & ~(Filters.command | Filters.regex('^хто\?$')), echo))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^хто\?$'), bind))
+
+    updater.start_polling()
+    updater.idle()
+
+
+#TODO: Каждый может про себя что-то написать через бота (не пожелание)
+#TODO: В отвте вместе с именем передавать telegram_id
+#TODO: Комнаты для секретных подарков (разные списки)
+
 
 if __name__ == '__main__':
-    print(DATA_DIR)
-    print(TOKEN)
+    main()
+
+
